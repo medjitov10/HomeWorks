56839813b1b96a7d6fdc804b86d84e16f8be568b
Osman Medzhytov <medjitov10@gmail.com>
2017-10-10 17:24:08 -0400

    closure and event

---
+++ w5d5/index.js
@@ -0,0 +1,16 @@
+window.setTimeout(() => {
+  alert('hammertime');
+}, 5000)
+
+
+
+function hummerTime(time){
+
+  window.setTimeout(() =>{
+    console.log(`${time} is hummerTime`);
+  })
+
+}
+
+
+hummerTime(4)
